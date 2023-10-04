be9fd5d92b5e2d7154a104f869b9bcbd5610c602
Mark Settarov <2top4live@gmail.com>
2023-10-04 13:49:26 +0300

    Add main.py and .env_example

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
+import time
+
 from data.binance_spot import BinanceSpotDataSource
+from notifications.telegram import TelegramNotifier
+from signals.macd import MacdSignal
+from visualisations.macd_visualizer import MACDVisualization
 
 if __name__ == '__main__':
+
     cl = BinanceSpotDataSource()
-    print(cl.connection_status())
-    print(cl.get_data())
+    macd_signal = MacdSignal()
+    messenger = TelegramNotifier()
+
+    print('Bot started')
+
+    while True:
+        df = cl.get_data()
+        signal = macd_signal.identify_signal(df)
+
+        if signal.direction != 'none':
+            visualization = MACDVisualization()
+            image_path = visualization.visualize(df, signal)
+            messenger.send(signal, image_path)
+
+            print(signal)
+
+        time.sleep(60)
